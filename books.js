8023d6f46962a373d38040571c5e7b44c0d2a0a8
younus9985 <123713844+younus9985@users.noreply.github.com>
2023-01-28 09:47:44 +0530

    Add files via upload

---
+++ books.js
@@ -0,0 +1,23 @@
+import img1 from "./images/book1.jpg";
+import img2 from "./images/book2.jpg";
+import img3 from "./images/book-3.jpg";
+export const books = [
+  {
+    author: "Chetan Bhagat",
+    title: "The Girl In The Room 105",
+    img: img1,
+    id: 1,
+  },
+  {
+    author: "Chetan Bhagat",
+    title: "One Indian Girl",
+    img: img2,
+    id: 2,
+  },
+  {
+    author: "Chetan Bhagat",
+    title: "2 States",
+    img: img3,
+    id: 3,
+  },
+];
